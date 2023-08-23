da22e67bcb365638e30080b80cfdc0d5e4045e50
Nikita Teremovskiy <ni-2@mail.ru>
2023-08-23 16:35:50 +0300

    feat: add GitHub adapter (DAP-3927)

---
+++ tests/scenarios/github-config-post.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@dapplets/dapplet-playwright'
+
+const registry = 'http://localhost:3000/dapplet.json'
+
+test('should inject widgets in GitHub comment', async ({
+    page,
+    skipOnboarding,
+    enableDevServer,
+    activateDapplet,
+}) => {
+    await page.goto(process.env.GITHUB_TEST_POST_URL!)
+    await skipOnboarding()
+    await enableDevServer(registry)
+    await activateDapplet('github-config-demo.dapplet-base.eth', registry)
+
+    const badges = await page.getByTitle('POST_AVATAR_BADGE').all()
+    expect(badges.length).toBe(16)
+    for (const li of badges) await expect(li).toBeAttached()
+
+    const buttons = await page.getByTitle('POST_BUTTON').all()
+    expect(buttons.length).toBe(16)
+    for (const li of buttons) await expect(li).toBeAttached()
+})
---
+++ tests/scenarios/github-config-profile.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@dapplets/dapplet-playwright'
+
+const registry = 'http://localhost:3000/dapplet.json'
+
+test('should inject widgets in GitHub profile', async ({
+    page,
+    skipOnboarding,
+    enableDevServer,
+    activateDapplet,
+}) => {
+    await page.goto(process.env.GITHUB_TEST_PROFILE_URL!)
+    await skipOnboarding()
+    await enableDevServer(registry)
+    await activateDapplet('github-config-demo.dapplet-base.eth', registry)
+
+    const badges = await page.getByTitle('PROFILE_AVATAR_BADGE').all()
+    expect(badges.length).toBe(4)
+    for (const li of badges) await expect(li).toBeAttached()
+
+    const buttons = await page.getByTitle('PROFILE_BUTTON').all()
+    expect(buttons.length).toBe(6)
+    for (const li of buttons) await expect(li).toBeAttached()
+})
