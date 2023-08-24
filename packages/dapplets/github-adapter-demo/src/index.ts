336b1c380142bdb3bddef6aff98b430109c360d9
Nikita Teremovskiy <ni-2@mail.ru>
2023-08-24 13:05:09 +0300

    fix: add normalize-space() to GitHub context builders

--- packages/dapplets/github-adapter-demo/src/index.ts
+++ packages/dapplets/github-adapter-demo/src/index.ts
@@ -12,108 +12,99 @@ export default class DemoDapplet implements IFeature {
         const { avatarBadge, button } = this.adapter.exports
 
         this.adapter.attachConfig({
-            PROFILE: (context) => {
-                const ctx = Object.fromEntries(
-                    Object.entries(context).map(([key, value]) => {
-                        let newValue = value
-                        if (typeof value === 'string') newValue = value.trim()
-                        return [key, newValue]
-                    })
-                )
-                return [
-                    avatarBadge({
-                        initial: 'DEFAULT',
-                        DEFAULT: {
-                            vertical: 'bottom',
-                            horizontal: 'right',
-                            img: MAIN_IMG,
-                            tooltip: 'PROFILE_AVATAR_BADGE',
-                            exec: () => console.log(ctx),
-                        },
-                    }),
-                    avatarBadge({
-                        initial: 'DEFAULT',
-                        DEFAULT: {
-                            vertical: 'bottom',
-                            horizontal: 'left',
-                            img: MAIN_IMG,
-                            tooltip: 'PROFILE_AVATAR_BADGE',
-                            exec: () => console.log(ctx),
-                        },
-                    }),
-                    avatarBadge({
-                        initial: 'DEFAULT',
-                        DEFAULT: {
-                            vertical: 'top',
-                            horizontal: 'right',
-                            img: MAIN_IMG,
-                            tooltip: 'PROFILE_AVATAR_BADGE',
-                        },
-                    }),
-                    avatarBadge({
-                        initial: 'DEFAULT',
-                        DEFAULT: {
-                            vertical: 'top',
-                            horizontal: 'left',
-                            img: MAIN_IMG,
-                            tooltip: 'PROFILE_AVATAR_BADGE',
-                        },
-                    }),
-                    button({
-                        initial: 'DEFAULT',
-                        DEFAULT: {
-                            img: MAIN_IMG,
-                            label: 'TEST',
-                            tooltip: 'PROFILE_BUTTON',
-                            exec: () => console.log(ctx),
-                        },
-                    }),
-                    button({
-                        initial: 'DEFAULT',
-                        DEFAULT: {
-                            img: MAIN_IMG,
-                            basic: true,
-                            label: 'BASIC',
-                            tooltip: 'PROFILE_BUTTON',
-                            exec: () => console.log(ctx),
-                        },
-                    }),
-                    button({
-                        initial: 'DEFAULT',
-                        DEFAULT: {
-                            label: 'TEST',
-                            tooltip: 'PROFILE_BUTTON',
-                            exec: () => console.log(ctx),
-                        },
-                    }),
-                    button({
-                        initial: 'DEFAULT',
-                        DEFAULT: {
-                            basic: true,
-                            label: 'BASIC',
-                            tooltip: 'PROFILE_BUTTON',
-                            exec: () => console.log(ctx),
-                        },
-                    }),
-                    button({
-                        initial: 'DEFAULT',
-                        DEFAULT: {
-                            img: MAIN_IMG,
-                            tooltip: 'PROFILE_BUTTON',
-                            exec: () => console.log(ctx),
-                        },
-                    }),
-                    button({
-                        initial: 'DEFAULT',
-                        DEFAULT: {
-                            img: MAIN_IMG,
-                            basic: true,
-                            tooltip: 'PROFILE_BUTTON',
-                            exec: () => console.log(ctx),
-                        },
-                    }),
-                ]
-            },
+            PROFILE: (ctx) => [
+                avatarBadge({
+                    initial: 'DEFAULT',
+                    DEFAULT: {
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                        img: MAIN_IMG,
+                        tooltip: 'PROFILE_AVATAR_BADGE',
+                        exec: () => console.log(ctx),
+                    },
+                }),
+                avatarBadge({
+                    initial: 'DEFAULT',
+                    DEFAULT: {
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                        img: MAIN_IMG,
+                        tooltip: 'PROFILE_AVATAR_BADGE',
+                        exec: () => console.log(ctx),
+                    },
+                }),
+                avatarBadge({
+                    initial: 'DEFAULT',
+                    DEFAULT: {
+                        vertical: 'top',
+                        horizontal: 'right',
+                        img: MAIN_IMG,
+                        tooltip: 'PROFILE_AVATAR_BADGE',
+                    },
+                }),
+                avatarBadge({
+                    initial: 'DEFAULT',
+                    DEFAULT: {
+                        vertical: 'top',
+                        horizontal: 'left',
+                        img: MAIN_IMG,
+                        tooltip: 'PROFILE_AVATAR_BADGE',
+                    },
+                }),
+                button({
+                    initial: 'DEFAULT',
+                    DEFAULT: {
+                        img: MAIN_IMG,
+                        label: 'TEST',
+                        tooltip: 'PROFILE_BUTTON',
+                        exec: () => console.log(ctx),
+                    },
+                }),
+                button({
+                    initial: 'DEFAULT',
+                    DEFAULT: {
+                        img: MAIN_IMG,
+                        basic: true,
+                        label: 'BASIC',
+                        tooltip: 'PROFILE_BUTTON',
+                        exec: () => console.log(ctx),
+                    },
+                }),
+                button({
+                    initial: 'DEFAULT',
+                    DEFAULT: {
+                        label: 'TEST',
+                        tooltip: 'PROFILE_BUTTON',
+                        exec: () => console.log(ctx),
+                    },
+                }),
+                button({
+                    initial: 'DEFAULT',
+                    DEFAULT: {
+                        basic: true,
+                        label: 'BASIC',
+                        tooltip: 'PROFILE_BUTTON',
+                        exec: () => console.log(ctx),
+                    },
+                }),
+                button({
+                    initial: 'DEFAULT',
+                    DEFAULT: {
+                        img: MAIN_IMG,
+                        tooltip: 'PROFILE_BUTTON',
+                        exec: () => console.log(ctx),
+                    },
+                }),
+                button({
+                    initial: 'DEFAULT',
+                    DEFAULT: {
+                        img: MAIN_IMG,
+                        basic: true,
+                        tooltip: 'PROFILE_BUTTON',
+                        exec: () => console.log(ctx),
+                    },
+                }),
+            ],
             POST: (ctx) => [
                 avatarBadge({
                     initial: 'DEFAULT',
